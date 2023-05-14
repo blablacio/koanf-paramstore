10599b857c4444438687133a5d2ba8caf2feb8b6
Vladislav Manchev <v@bin.bz>
2023-05-14 11:32:03 +0300

    Initial commit

---
+++ paramstore.go
@@ -0,0 +1,184 @@
+package paramstore
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/knadh/koanf/maps"
+)
+
+type Config struct {
+	Delimiter          string
+	Path               string
+	WithDecryption     bool
+	AWSAccessKeyID     string
+	AWSSecretAccessKey string
+	AWSRoleARN         string
+	AWSRegion          string
+	WatchInterval      time.Duration
+}
+
+type ParamStore struct {
+	client *ssm.Client
+	config Config
+	input  ssm.GetParametersByPathInput
+	params []types.Parameter
+	cb     func(s string) string
+}
+
+func Provider(cfg Config, cb func(s string) string) *ParamStore {
+	// Load the default config
+	c, err := config.LoadDefaultConfig(context.Background())
+
+	if err != nil {
+		return nil
+	}
+
+	if cfg.Delimiter == "" {
+		cfg.Delimiter = "/"
+	}
+
+	if cfg.AWSRegion != "" {
+		c.Region = cfg.AWSRegion
+	}
+
+	// Check if AWS access key ID and secret key are specified
+	if cfg.AWSAccessKeyID != "" && cfg.AWSSecretAccessKey != "" {
+		c.Credentials = credentials.NewStaticCredentialsProvider(cfg.AWSAccessKeyID, cfg.AWSSecretAccessKey, "")
+	}
+
+	// Check if AWS role ARN is present
+	if cfg.AWSRoleARN != "" {
+		stsSvc := sts.NewFromConfig(c)
+		credentials := stscreds.NewAssumeRoleProvider(stsSvc, cfg.AWSRoleARN)
+		c.Credentials = aws.NewCredentialsCache(credentials)
+	}
+
+	client := ssm.NewFromConfig(c)
+
+	return &ParamStore{client: client, config: cfg, cb: cb}
+}
+
+func ProviderWithClient(cfg Config, cb func(s string) string, client *ssm.Client) *ParamStore {
+	return &ParamStore{client: client, config: cfg, cb: cb}
+}
+
+func (ps *ParamStore) Read() (map[string]interface{}, error) {
+	// Check if path is provided
+	if ps.config.Path == "" {
+		return nil, errors.New("no parameter path provided")
+	}
+
+	// Set SSM API call input
+	ps.input = ssm.GetParametersByPathInput{
+		Path:           aws.String(ps.config.Path),
+		WithDecryption: &ps.config.WithDecryption,
+	}
+
+	// Get parameters
+	var params []types.Parameter
+
+	for {
+		result, err := ps.client.GetParametersByPath(context.Background(), &ps.input)
+
+		if err != nil {
+			return nil, err
+		}
+
+		params = append(params, result.Parameters...)
+
+		ps.input.NextToken = result.NextToken
+
+		if result.NextToken == nil {
+			break
+		}
+
+	}
+
+	ps.params = params
+
+	mp := make(map[string]interface{})
+
+	for _, param := range params {
+		key := *param.Name
+
+		// Transform key if transformer is provided
+		if ps.cb != nil {
+			key = ps.cb(key)
+		}
+
+		if key == "" {
+			return nil, errors.New("transformed key is empty")
+		}
+
+		// Set key value
+		mp[key] = param.Value
+	}
+
+	return maps.Unflatten(mp, ps.config.Delimiter), nil
+}
+
+func (ps *ParamStore) ReadBytes() ([]byte, error) {
+	return nil, errors.New("paramstore provider does not support ReadBytes method")
+}
+
+func (ps *ParamStore) Watch(cb func(event interface{}, err error)) error {
+	// Initialize watch interval
+	if ps.config.WatchInterval == 0 {
+		ps.config.WatchInterval = 600 * time.Second
+	}
+
+	go func() {
+		// Start new ticker
+		ticker := time.NewTicker(ps.config.WatchInterval)
+		defer ticker.Stop()
+
+	main:
+		for range ticker.C {
+			// Initialize parameters slice
+			var params []types.Parameter
+
+			// Fetch all parameters from API
+			for {
+				result, err := ps.client.GetParametersByPath(context.Background(), &ps.input)
+
+				if err != nil {
+					cb(nil, err)
+
+					continue main
+				}
+
+				params = append(params, result.Parameters...)
+
+				ps.input.NextToken = result.NextToken
+
+				if result.NextToken == nil {
+					break
+				}
+			}
+
+			// Check for changes
+			for _, newParam := range params {
+				// Find parameter in previously saved parameters
+				for _, p := range ps.params {
+					if *p.ARN == *newParam.ARN && newParam.Version != p.Version {
+						// Trigger update
+						cb(nil, nil)
+
+						continue main
+					}
+				}
+			}
+		}
+	}()
+
+	return nil
+}
